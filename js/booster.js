e8baf41817d8e4a189d577f1f6fbed5b49353949
SrGokuto <jeonardoelfaker@gmail.com>
2025-10-08 14:51:07 -0500

    Add opening animations (Temporary [i'm loosing my mind])

--- js/booster.js
+++ js/booster.js
@@ -1,11 +1,21 @@
 function getByID(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 function getByClass(className) {
-    return document.querySelector(className);
+  return document.querySelector(className);
 }
 
-getByClass('.booster-pack').addEventListener('click', function() {
-    getByClass('.open-btn').classList.toggle('show');
-    this.classList.toggle('selected');
+const projectsPack = getByID("projects-pack");
+getByClass(".booster-pack").addEventListener("click", function () {
+  getByClass(".open-btn").classList.toggle("show");
+  this.classList.toggle("selected");
+});
+getByClass(".open-btn").addEventListener("click", function () {
+  projectsPack.style.transition = "0.5s cubic-bezier(0.83, 0.82, 0, 0.99)";
+  projectsPack.style.position = "absolute";
+  projectsPack.style.transform =
+    "translate(calc(50vw - 90px), calc(50vh - 260px))";
+  setTimeout(() => {
+    projectsPack.style.animation = "vibrate 0.3s infinite";
+  }, 500);
 });
